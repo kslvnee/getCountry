a8f05ec397b466d03fa15dc6525091c7a8d83da0
MaratMingazov <maratmingazovr@gmail.com>
2018-05-12 16:36:09 +0300

    First stable reliz

--- src/main/kotlin/getcountry/GetCountry.kt
+++ src/main/kotlin/getcountry/GetCountry.kt
@@ -1,18 +1,39 @@
-@file:JvmName("GetCountry2")
-
 package getcountry
 
 import com.google.gson.GsonBuilder
 import java.io.File
 import java.util.regex.Pattern
-import javax.swing.plaf.synth.SynthLookAndFeel.getRegion
 import java.util.ArrayList
 
-
-
-
+/**
+ * List of all countries with regions and cities
+ */
 val countries : List<Country> = getOriginalCountries()
 
+/**
+ * This method return the list of all countries
+ * Countries stored in resources/countries
+ * Every country presented in form of class Country
+ *
+ * @return List<Country> the list of all countries
+ * @see Country
+ */
+fun getOriginalCountries() : List<Country> {
+    val listOfFiles = File("src/main/resources/countries").listFiles()
+    val countries : MutableList<Country> = ArrayList()
+    for (file in listOfFiles) {
+        countries.add(GsonBuilder().create().fromJson(file.readText(), Country::class.java))
+    }
+    return countries
+}
+
+/**
+ * This method tries to determine the Country/Region/City names
+ * based on string location.
+ *
+ * @return List<IResultCountry> the list of all found country/region/city names
+ * @see IResultCountry
+ */
 fun get(location:String):List<IResultCountry> {
     if (location.isEmpty()) {
         return ArrayList()
@@ -82,11 +103,3 @@ fun get(location:String):List<IResultCountry> {
     return results
 }
 
-fun getOriginalCountries() : List<Country> {
-    val listOfFiles = File("src/main/resources/countries").listFiles()
-    val countries : MutableList<Country> = ArrayList()
-    for (file in listOfFiles) {
-            countries.add(GsonBuilder().create().fromJson(file.readText(), Country::class.java))
-    }
-    return countries
-}
